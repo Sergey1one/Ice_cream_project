34145d75eb36e58bbf2364a0c30ab809f651225b
Саша Маслак <owmaslak@gmail.com>
2022-04-05 12:47:26 +0200

    bagfix - modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,20 +5,20 @@
     modal: document.querySelector("[data-modal]"),
   };
 
-//   refs.openModalBtn.addEventListener("click", toggleModal);
-//   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
   }
 })();
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-product1-open]"),
-    closeModalBtn: document.querySelector("[data-modal-product1-close]"),
-    modal: document.querySelector("[data-modal-product1]"),
-  };
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-product1-open]"),
+//     closeModalBtn: document.querySelector("[data-modal-product1-close]"),
+//     modal: document.querySelector("[data-modal-product1]"),
+//   };
 
 //   refs.openModalBtn.addEventListener("click", toggleModal);
 //   refs.closeModalBtn.addEventListener("click", toggleModal);
@@ -43,17 +43,17 @@
 //   }
 // })();
 
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-product3-open]"),
-    closeModalBtn: document.querySelector("[data-modal-product3-close]"),
-    modal: document.querySelector("[data-modal-product3]"),
-  };
+// (() => {
+//   const refs = {
+//     openModalBtn: document.querySelector("[data-modal-product3-open]"),
+//     closeModalBtn: document.querySelector("[data-modal-product3-close]"),
+//     modal: document.querySelector("[data-modal-product3]"),
+//   };
 
 //   refs.openModalBtn.addEventListener("click", toggleModal);
 //   refs.closeModalBtn.addEventListener("click", toggleModal);
 
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
-})();
+//   function toggleModal() {
+//     refs.modal.classList.toggle("is-hidden");
+//   }
+// })();
